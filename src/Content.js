c58487be7a9c5475623807a2ba01a829374ea138
rahulfuturecoreinnovations <rahul.futurecoreinnovations@gmail.com>
2024-09-19 12:35:34 +0530

    some minor changes

--- src/Content.js
+++ src/Content.js
@@ -19,11 +19,11 @@ import services_logo1 from "./assets/images/Services/logo1.png";
 import services_logo2 from "./assets/images/Services/logo2.png";
 import mongodata from "./assets/images/Services/mongodata.png";
 
-import project1 from "./assets/images/projects/img1.png";
-import project2 from "./assets/images/projects/img2.png";
-import project3 from "./assets/images/projects/img3.png";
-import project4 from "./assets/images/projects/img4.png";
-import person_project from "./assets/images/projects/person.png";
+import project1 from "./assets/images/Projects/img1.png";
+import project2 from "./assets/images/Projects/img2.png";
+import project3 from "./assets/images/Projects/img3.png";
+import project4 from "./assets/images/Projects/img4.png";
+import person_project from "./assets/images/Projects/person.png";
 
 
 import clg from "./assets/images/Testimonials/clg.png";
