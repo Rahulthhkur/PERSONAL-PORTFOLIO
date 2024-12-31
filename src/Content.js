659da938ab0eae48319944547446f2299351ef79
Rahulthhkur <rthakur1233@gmail.com>
2024-12-31 20:48:25 +0530

    checked

--- src/Content.js
+++ src/Content.js
@@ -156,7 +156,7 @@ export const content = {
     image: person_project,
     project_content: [
       {
-        title: "ReaL Estate Pro q",
+        title: "ReaL Estate Pro",
         image: project1,
         link: "http://real-estate-pro-m4wj.onrender.com/",
       },
